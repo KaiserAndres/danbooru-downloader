1f4d8326a92d6bbf93fd4e6a81f048c476b5dd1f
Andres Villagra <andres_vf@icloud.com>
2018-03-30 17:11:19 -0300

    Implemented the Post data class.

--- src/main/java/com/kai/danbooruManager/DanbooruPage.java
+++ src/main/java/com/kai/danbooruManager/DanbooruPage.java
@@ -40,4 +40,7 @@ public class DanbooruPage {
         }
     }
 
+    public ArrayList<Post> getPosts() {
+        return posts;
+    }
 }
--- src/main/java/com/kai/danbooruManager/Post.java
+++ src/main/java/com/kai/danbooruManager/Post.java
@@ -1,14 +1,42 @@
 package com.kai.danbooruManager;
 
-
 import org.jsoup.nodes.Element;
 
+import java.util.Arrays;
+import java.util.Map;
+import static java.lang.Integer.parseInt;
+
 public class Post {
-    private Element element;
+
+    private int id;
+    private String[] tags;
+    private int width;
+    private int height;
+    private String source;
+    private String fileUrl;
+    private String largeFileUrl;
 
     public Post(Element element) {
-        this.element = element;
+        Map<String, String> elementData = element.dataset();
+        id = parseInt(elementData.get("id"));
+        width = parseInt(elementData.get("width"));
+        height = parseInt(elementData.get("height"));
+        source = elementData.get("source");
+        fileUrl = elementData.get("file-url");
+        largeFileUrl = elementData.get("large-file-url");
+        tags = elementData.get("tags").split(" ");
     }
 
-
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", tags=" + (tags == null ? null : Arrays.asList(tags)) + '\n' +
+                ", width=" + width +
+                ", height=" + height +
+                ", source='" + source + '\'' +
+                ", fileUrl='" + fileUrl + '\'' +
+                ", largeFileUrl='" + largeFileUrl + '\'' +
+                '}';
+    }
 }
