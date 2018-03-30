5fb73ec38cc2f58443b3b7e0c213f79aa7ab0859
Andres Villagra <andres_vf@icloud.com>
2018-03-30 16:53:12 -0300

    Implemented page page handling.

---
+++ src/main/java/com/kai/danbooruManager/DanbooruPage.java
@@ -0,0 +1,43 @@
+package com.kai.danbooruManager;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public class DanbooruPage {
+
+    private Logger pageLogger;
+    private ArrayList<Post> posts;
+    private Url url;
+    private Document doc;
+
+    public DanbooruPage(Url pageUrl) {
+        pageLogger = Logger.getLogger(pageUrl.toString());
+        posts = new ArrayList<Post>();
+        this.url = pageUrl;
+        this.doc = null;
+    }
+
+    public void downloadPage() {
+        try {
+            doc = Jsoup.connect(url.toString()).get();
+        } catch (IOException e) {
+            pageLogger.severe("Unable to downloadPage page: " + url.toString());
+        }
+    }
+
+    public void populatePosts() {
+        Element body = doc.body();
+        Elements detectedArticles = body.getElementsByTag("article");
+        for (Element article : detectedArticles) {
+            posts.add(new Post(article));
+        }
+    }
+
+}
---
+++ src/main/java/com/kai/danbooruManager/Post.java
@@ -0,0 +1,14 @@
+package com.kai.danbooruManager;
+
+
+import org.jsoup.nodes.Element;
+
+public class Post {
+    private Element element;
+
+    public Post(Element element) {
+        this.element = element;
+    }
+
+
+}
