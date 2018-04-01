3b493dd90dc44703ba1db8564905a2818e141385
Andres Villagra <andres_vf@icloud.com>
2018-04-01 18:28:22 -0300

    Added main class to the POM, made the main class do something.

--- src/main/java/com/kai/Main.java
+++ src/main/java/com/kai/Main.java
@@ -1,7 +1,29 @@
 package com.kai;
 
+import com.kai.danbooruManager.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Configuration cfg = new Configuration();
+        cfg.addDesiredCategory("fujiwara_no_mokou");
+        cfg.addForbiddenCategory("comic");
+        cfg.addForbiddenCategory("nude");
+        cfg.setImageTarget("/Users/kaiser/Pictures/test/mokou");
+
+        DownloadManager manager = new DownloadManager(cfg);
+
+        Url myUrl = new Url("https://hijiribe.donmai.us", cfg);
+
+        for (int i=10; i<13; i++) {
+            System.out.println("Checking page Nº"+i);
+            myUrl.setPageNumber(i);
+            DanbooruPage page = new DanbooruPage(myUrl);
+            page.downloadPage();
+            page.populatePosts();
+
+            manager.addPosts(page.getPosts());
+        }
+
+        while (!manager.isEmpty()) { manager.download(); }
     }
 }
