4cc8a5eb0950ea3633ff7d59d0659e30ee7716a3
Andres Villagra <andres_vf@icloud.com>
2018-04-12 20:12:33 -0300

    updated version number

--- src/main/java/com/kai/booter/Main.java
+++ src/main/java/com/kai/booter/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main(String[] args) {
 
         String programName = "Danbooru downloader";
-        String versionNumber = "1.1";
+        String versionNumber = "1.2";
 
         Options cliOptions = CLIOptionCreator.getOptions();
         CommandLineParser parser = new DefaultParser();
